e0d67477ff72373cf786367d7b38047bd8230f7a
Daniel Vasilev <85338748+ddvasilev20@users.noreply.github.com>
2022-11-20 14:56:15 +0200

    Add arrays with questions about Bulgaria and Germany

---
+++ game.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <windows.h>
+#include <conio.h>
+
+
+using namespace std;
+
+int main()
+{
+  string allQuestionsBulgaria[10] = {    
+	"Which is the smallest country?",
+	"Which is the capital of Argentina?",
+	"Which is called world population day?",
+	"Who is the first person to draw the map of earth?",
+	"Who was the first lady astronaut?",
+	"Who is the father of chemistry?",
+	"Who is the inventor of Rail Engine?",
+	"Which is the capital of America?",
+	"Which is not the member of G-8?",
+	"Which is the biggest ocean?" };
+  
+  
+  string allQuestionsGermany[10] = {    // all the questions
+	"Which is the smallest country?",
+	"Which is the capital of Argentina?",
+	"Which is called world population day?",
+	"Who is the first person to draw the map of earth?",
+	"Who was the first lady astronaut?",
+	"Who is the father of chemistry?",
+	"Who is the inventor of Rail Engine?",
+	"Which is the capital of America?",
+	"Which is not the member of G-8?",
+	"Which is the biggest ocean?" };
+}
