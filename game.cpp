3a1c60ba656ec18a9403da41129c553ac68ca1d4
Daniel Vasilev <85338748+ddvasilev20@users.noreply.github.com>
2022-11-20 14:57:04 +0200

    Add array with questions about Spain

--- game.cpp
+++ game.cpp
@@ -31,4 +31,16 @@ int main()
 	"Which is the capital of America?",
 	"Which is not the member of G-8?",
 	"Which is the biggest ocean?" };
+	
+	string allQuestionsSpain[10] = {    // all the questions
+	"Which is the smallest country?",
+	"Which is the capital of Argentina?",
+	"Which is called world population day?",
+	"Who is the first person to draw the map of earth?",
+	"Who was the first lady astronaut?",
+	"Who is the father of chemistry?",
+	"Who is the inventor of Rail Engine?",
+	"Which is the capital of America?",
+	"Which is not the member of G-8?",
+	"Which is the biggest ocean?" };
 }
